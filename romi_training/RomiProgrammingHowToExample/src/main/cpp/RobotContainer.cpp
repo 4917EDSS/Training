14ab92e4ffae60709f2a32615499294b668ab783
Eric Gaudet <ericjpgaudet@yahoo.ca>
2021-12-06 16:20:13 -0500

    Started Romi version of the code that's built using the 4917's FRC Programming How To's document (v5)

---
+++ romi_training/RomiProgrammingHowToExample/src/main/cpp/RobotContainer.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "RobotContainer.h"
+
+RobotContainer::RobotContainer() : m_autonomousCommand(&m_subsystem) {
+  // Initialize all of your commands and subsystems here
+
+  // Configure the button bindings
+  ConfigureButtonBindings();
+}
+
+void RobotContainer::ConfigureButtonBindings() {
+  // Configure controller joystick axes
+  m_driverController.SetXChannel(0);
+  m_driverController.SetYChannel(1);
+  m_driverController.SetZChannel(2);
+  m_driverController.SetThrottleChannel(3);
+
+  m_operatorController.SetXChannel(0);
+  m_operatorController.SetYChannel(1);
+  m_operatorController.SetZChannel(2);
+  m_operatorController.SetThrottleChannel(3);
+}
+
+frc2::Command* RobotContainer::GetAutonomousCommand() {
+  // An example command will be run in autonomous
+  return &m_autonomousCommand;
+}
