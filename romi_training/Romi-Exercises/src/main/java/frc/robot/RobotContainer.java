d564abb7499a1f421fe077b0d41b02024d0b35aa
Eric Gaudet <ericjpgaudet@yahoo.ca>
2024-12-31 14:03:52 -0500

    Clean Romi Exercise Project

--- romi_training/Romi-Exercises/src/main/java/frc/robot/RobotContainer.java
+++ romi_training/Romi-Exercises/src/main/java/frc/robot/RobotContainer.java
@@ -4,48 +4,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.DrivetrainSub;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
- * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
- * Instead, the structure of the robot (including subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final DrivetrainSub m_drivetrainSub = new DrivetrainSub(); // This instantiates the subsystem so you can use it
-
   private final CommandPS4Controller m_driverController =
       new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
-
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-
-    m_drivetrainSub.setDefaultCommand(new RunCommand(
-        () -> m_drivetrainSub.drive(-m_driverController.getLeftY(), -m_driverController.getRightY()), m_drivetrainSub));
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by instantiating a
-   * {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link edu.wpi.first.wpilibj.Joystick} or
-   * {@link XboxController}), and then passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {
-    m_driverController.cross().onTrue(new PrintCommand("Cross pressed!"));
-
-    m_driverController.circle().whileTrue(new StartEndCommand(() -> m_drivetrainSub.drive(0.25, 0.25),
-        () -> m_drivetrainSub.drive(0.0, 0.0), m_drivetrainSub));
-
-  }
+  private void configureButtonBindings() {}
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
