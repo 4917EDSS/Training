56c9d6a5151411d8f688c689f419fb4ceb4e4181
Eric Gaudet <ericjpgaudet@yahoo.ca>
2025-01-02 10:36:07 -0500

    Added the completed 2024 Romi project

---
+++ romi_training/Romi-Exercises-Complete/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.DrivetrainSub;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final DrivetrainSub m_drivetrainSub = new DrivetrainSub(); // This instantiates the subsystem so you can use it
+
+  private final CommandPS4Controller m_driverController =
+      new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+
+    m_drivetrainSub.setDefaultCommand(new RunCommand(
+        () -> m_drivetrainSub.drive(-m_driverController.getLeftY(), -m_driverController.getRightY()),
+        m_drivetrainSub));
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick}, and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    m_driverController.cross().onTrue(new PrintCommand("Cross pressed!"));
+
+    m_driverController.circle().whileTrue(new StartEndCommand(
+        () -> m_drivetrainSub.drive(0.25, 0.25),
+        () -> m_drivetrainSub.drive(0.0, 0.0),
+        m_drivetrainSub));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return new PrintCommand("No auto coded yet");
+  }
+}
---
+++ romi_training/Romi-Exercises-Complete/src/main/java/frc/robot/subsystems/DrivetrainSub.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DrivetrainSub extends SubsystemBase {
+
+  private final Spark m_leftMotor = new Spark(Constants.PwmIds.kLeftMotor);
+  private final Spark m_rightMotor = new Spark(Constants.PwmIds.kRightMotor);
+
+  /** Creates a new DrivetrainSub. */
+  public DrivetrainSub() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void drive(double leftPower, double rightPower) {
+    System.out.println("Driving: L=" + leftPower + " R=" + rightPower); // See that this is being called correctly
+    m_leftMotor.set(leftPower);
+    m_rightMotor.set(-rightPower); // Since this motor is rotated 180 deg, reverse its power
+  }
+}
